fb062d4982da4c321d9926eb41b8f4660315afe3
PhilMinard <pminard1@outlook.com>
2022-03-28 21:49:11 +0100

    Fixed On/Off True/False values
    Removed unused code
    
    Re-added Servo code to uncomment on a Raspberry Pi 4

--- CamPCVisvis.py
+++ CamPCVisvis.py
@@ -4,10 +4,11 @@ import sys
 import os
 import pygubu
 import imageio
-import visvis as vv  # New
-# import picamera
-# from time import sleep
+# import visvis as vv  # New
 from PIL import Image, ImageTk
+# Uncomment on pi
+# from board import SCL, SDA
+# import busio
 
 try:
     import tkinter as tk
@@ -17,6 +18,36 @@ except:
     import tkMessageBox as messagebox
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
+
+# Import the PCA9685 module.
+# from adafruit_pca9685 import PCA9685
+# This uses Adafruit motor library, note: good for continuous rotation servos
+# from adafruit_motor import servo
+
+# i2c = busio.I2C(SCL, SDA)
+
+# Create a simple PCA9685 class instance.
+# pca = PCA9685(i2c)
+# pca.frequency = 50
+
+# servo1 = servo.Servo(pca.channels[0], min_pulse=580, max_pulse=2480)
+# servo2 = servo.Servo(pca.channels[1], min_pulse=580, max_pulse=2480)
+
+# The pulse range is 1000 - 2000 by default.
+# Re-add to work on the pi!!!!
+# servo1 = servo.Servo(pca.channels[0])
+# servo2 = servo.Servo(pca.channels[1])
+# servo1.angle = 50
+# servo2.angle = 50
+# for i in range(180):
+##    servo1.angle = i
+##for i in range(180):
+##    servo1.angle = 180 - i
+##pca.deinit()#I think this stops the signal after the move to save power
+#Adapt the bit above
+
+# servo1.angle = i
+# pca.deinit()
 
 
 class Myapp:
@@ -35,49 +66,40 @@ class Myapp:
     def on_Button_Show_clicked(self):
         """pressing the button to change between the puppy image
         and a self facing video feed"""
-        puppy_image = 'C:\\Users\\pminard\\Desktop\\Gubu\\CamPCVisvis\\Puppies.jpg'
+        puppy_image = 'C:\\Users\\gingu\\Desktop\\Gubu Saves\\CamPCVisvis\\asleep_puppy.jpg'
+        # puppy_image = 'C:\\The\\Folder\\You\\Save\\code_to\\asleep_puppy.jpg'
+        # puppy_image = 'User/The/Folder/You/Save/code_to_on_Linux/asleep_puppy.jpg'
         off_image = Image.open(puppy_image)
         off_image_update = off_image.resize((450, 350), Image.ANTIALIAS)
         # label = self.builder.get_object('labelPicture')
         label = self.builder.get_object('Label_Feed')
         self.is_on = not self.is_on
         if self.is_on:
-            # label = self.builder.get_object('Label_Feed')
+            print(self.is_on)
             video_name = '<video0>'
             video = imageio.get_reader(video_name)
 
             for image in video.iter_data():
-                if(not self.is_on):
+                if not self.is_on:
                     break
                 """video feed for the label"""
                 image_on = ImageTk.PhotoImage(Image.fromarray(image))
-                # frame_image = ImageTk.PhotoImage(Image.fromarray(image))
-                # print("image from array")
-                # label.config(image=frame_image)
                 label.config(image=image_on)
-                # label.config(image=video)
                 label.image = image_on
-                # print("label config (image_on)")
-                # self.is_on = False
                 label.update()
 
         else:
-            # stop the video feed
-            # see note at bottom
             show_image_off = ImageTk.PhotoImage(off_image_update)
             label.configure(image=show_image_off)
             label.image = show_image_off
-            # self.is_on = True
             print(self.is_on)
         label.update()
             
     def on_Button_Update_Tilt_clicked(self):
         """Used to update the tilt servos upper and lower limits"""
         Enter_Tilt_High = (self.builder.tkvariables['TiltHighEntryVar'].get())
-        Add_High = print(Enter_Tilt_High)
         
         Enter_Tilt_Low = (self.builder.tkvariables['TiltLowEntryVar'].get())
-        Add_Tilt_Low = print(Enter_Tilt_Low)
 
         scale = self.builder.get_object('Scale_Tilt')
         scale.configure({'from': int(Enter_Tilt_High)})
@@ -87,11 +109,8 @@ class Myapp:
 
     def on_Button_Update_Pan_clicked(self):
         """Used to update the Pan servos upper and lower limits"""
-        Enter_High = (self.builder.tkvariables['PanHighEntryVar'].get())
-        Add_High = print(Enter_High)
-        
-        Enter_Low = (self.builder.tkvariables['PanLowEntryVar'].get())
-        Add_Low = print(Enter_Low)
+        Enter_High = (self.builder.tkvariables['PanLowEntryVar'].get())
+        Enter_Low = (self.builder.tkvariables['PanHighEntryVar'].get())
 
         scale = self.builder.get_object('Scale_Pan')
         scale.configure({'from': int(Enter_High)})
@@ -99,9 +118,9 @@ class Myapp:
         scale = self.builder.get_object('Scale_Pan')
         scale.configure({'to': int(Enter_Low)})
 
-    def entry_invalid(self):
-        """to show if incorrect value entered"""
-        messagebox.showinfo('Title', 'Invalid entry input')
+    # def entry_invalid(self):
+    #     """to show if incorrect value entered"""
+    #     messagebox.showinfo('Title', 'Invalid entry input')
 
     def on_scale1_changed(self, event):  # This matches the command for scale1
         """When the slider the scale1 value is updated to match"""
@@ -126,16 +145,3 @@ if __name__ == '__main__':
     root = tk.Tk()
     app = Myapp(root)
     root.mainloop()
-
-# Note: To set the slider variables try self.slider.set(100) OR
-# self.slider.From_.set(100)/self.slider.To:.set(100) maybe
-
-# root = tk.Tk()
-#     my_label = tk.Label(root)
-#     my_label.pack()
-#     thread = threading.Thread(target=stream, args=(my_label,))
-#     thread.daemon = 1
-#     thread.start()
-#     root.mainloop()
-
-# Also see https://stackoverflow.com/questions/36635567/tkinter-inserting-video-into-window
